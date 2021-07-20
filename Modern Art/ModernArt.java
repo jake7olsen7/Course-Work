7ff0252b9054b1a5e33a297595e283653edf2915
jake7olsen7 <30609434+jake7olsen7@users.noreply.github.com>
2021-07-20 16:26:28 -0600

    Modern Art Upload

---
+++ Modern Art/ModernArt.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jake
+ *
+ */
+public class ModernArt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
